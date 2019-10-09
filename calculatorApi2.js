fc7a7c77991ea2c858097a1d0aa84e7fd28d3590
DouglasMacKrell <douglasmackrell@pursuit.org>
2019-10-09 10:58:48 -0400

    submit

---
+++ calculatorApi2.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
+
+app.use(cors());
+
+app.get("/:op/:num1/:num2", (req, res) => {
+    let operator = req.params.op
+    let num1 = req.params.num1;
+    let num2 = req.params.num2;
+
+    let answer = {
+        num1: num1,
+        num2: num2,
+        result: parseInt(num1) + operator + parseInt(num2)
+    }
+
+    res.set("Content-Type", "application/json")
+    res.status(200)
+    res.json(answer)
+})
