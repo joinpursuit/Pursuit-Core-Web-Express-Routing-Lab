8b57ae8b47eb8323514948df158cbf124498dff1
Suzette <suzetteislam@gmail.com>
2019-10-08 21:49:41 -0400

    server

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -26,16 +26,58 @@ app.get('/add/:num1/:num2', (request, response)=>{
 
 })
 
-app.get('/sub', (request, response)=>{
+app.get('/sub/:num1/:num2', (request, response)=>{
+    let num1 = request.params.num1
+    let num2 = request.params.num2
+
+    const sub =(a,b)=>{
+        return a - b
+    }
+
+    let subJSON = {
+        first: num1,
+        second: num2,
+        result: sub(num1,num2)
+    }
+
+    response.json(subJSON)
+    response.status(200)
 
 })
 
-app.get('/mul', (request, response)=>{
+app.get('/mul/:num1/:num2', (request, response)=>{
+    let num1 = request.params.num1
+    let num2 = request.params.num2
 
+    const mul =(a, b)=>{
+        return a * b
+    }
+
+    let mulJSON ={
+        first: num1, 
+        second: num2, 
+        result: mul(num1, num2)
+    }
+    
+    response.json(mulJSON)
+    response.status(200)
 });
 
-app.get('/div', (request, response)=>{
+app.get('/div/:num1/:num2', (request, response)=>{
+    let num1 = request.params.num1
+    let num2 = request.params.num2
 
+    const div = (a,b)=>{
+        return (a / b)
+    }
+
+    let divJSON = {
+        first: num1, 
+        second: num2, 
+        result: div(num1, num2)
+    }
+    response.json(divJSON)
+    response.status(200)
 });
 
 app.listen(port, ()=>{
