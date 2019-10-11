0d0ef9fa10b9b44b37782d92186b0958bf4c8fbe
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-10-11 12:34:12 -0400

    v0.65, refactor Api1

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -7,118 +7,78 @@ const log = console.log;
 
 
 /* MODULE INITS */
+const path = require('path');
 const express = require('express');
+  const app = express();
+  const port = 8010;
 const cors = require('cors');
-
-
-const app = express();
-const port = 8010;
-
-
-app.use(cors());
-app.get("/", (req, res) => {
-  res.send("Root HIT!");
-});
-
-/* OPERATION ENDPOINTS */
-app.get("/add/:num1/:num2", (req, res) => {
-  const insObj = req.params;
-  if (numsCheck(insObj)) {
-    let result = number(insObj.num1) + number(insObj.num2);
-    let outputObj = {
-      num1: number(insObj.num1),
-      num2: number(insObj.num2),
-      operation: "add (num1 + num2)",
-      result: result,
-    }
-    res.json(outputObj);
-  } else {
-    res.send("Input Error: Please check your inputs (/num1/num2) and reload the page.");
-  }
-});
-
-app.get("/sub/:num1/:num2", (req, res) => {
-  const insObj = req.params;
-  if (numsCheck(insObj)) {
-    let result = number(insObj.num1) - number(insObj.num2);
-    let outputObj = {
-      num1: number(insObj.num1),
-      num2: number(insObj.num2),
-      operation: "subtract (num1 - num2)",
-      result: result,
-    }
-    res.json(outputObj);
-  } else {
-    res.send("Input Error: Please check your inputs (/num1/num2) and reload the page.");
-  }
-});
-
-app.get("/mul/:num1/:num2", (req, res) => {
-  const insObj = req.params;
-  if (numsCheck(insObj)) {
-    let result = number(insObj.num1) * number(insObj.num2);
-    let outputObj = {
-      num1: number(insObj.num1),
-      num2: number(insObj.num2),
-      operation: "multiply (num1 * num2)",
-      result: result,
-    }
-    res.json(outputObj);
-  } else {
-    res.send("Input Error: Please check your inputs (/num1/num2) and reload the page.");
-  }
-});
-
-app.get("/div/:num1/:num2", (req, res) => {
-  const insObj = req.params;
-  if (numsCheck(insObj)) {
-    let result = number(insObj.num1) / number(insObj.num2);
-    let outputObj = {
-      num1: number(insObj.num1),
-      num2: number(insObj.num2),
-      operation: "divide (num1 / num2)",
-      result: result,
-    }
-    res.json(outputObj);
-  } else {
-    res.send("Input Error: Please check your inputs (/num1/num2) and reload the page.");
-  }
-});
-
-
-/* NO-ROUTE-CATCH */
-app.use("*", (req, res) => {
-  res.status(404);
-  let html404 = `
-    <h3>Error 404</h3>
-    <h1>Why Hello There</h1>
-    <p>You took a wrong turn somewhere because now you're nowhere.</p>
-    <p>Use the following endpoints to get back to where you're going. Check your URL and try again.</p>
-    <ul>
-      <li><strong>to add: </strong>/add/num1/num2</li>
-      <li><strong>to subtract: </strong>/sub/num1/num2</li>
-      <li><strong>to multiply: </strong>/mul/num1/num2</li>
-      <li><strong>to divide: </strong>/div/num1/num2</li>
-    `;
-  res.send(html404);
-});
-
-/* HELPERS */
-const numsCheck = (paramsObj) => {
-  for (let input in paramsObj) {
-    if (isNaN(paramsObj[input])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const number = (input) => {
-  return parseInt(input, 10);
-}
-
+  app.use(cors());
 
 /* SERVER INIT */
 app.listen(port, () => {
-  log(`JoeyServer running on port ${port}. The sun will come out. Tomorrow.`);
+  log(`JoeyServer running on port ${port}. Good luck and godspeed.`);
 });
+// app.use(express.static(path.join(__dirname, 'errors')));
+
+
+/* SERVER MAINS */
+const ops = {
+  add: (a, b) => a + b,
+  sub: (a, b) => a - b,
+  mul: (a, b) => a * b,
+  div: (a, b) => a / b,
+}
+
+app.get("/:op/:num1/:num2", (req, res) => {
+  checkInputs(req, res);
+  let outputObj = {
+    num1: parseFloat(req.params.num1),
+    num2: parseFloat(req.params.num2),
+    operation: req.params.op,
+  }
+  outputObj.result = ops[req.params.op](outputObj.num1, outputObj.num2);
+  res.json(outputObj);
+});
+
+/* NO-ROUTE-CATCH */
+app.use("*", (req, res) => {
+  res.status(418);
+  res.send(error418);
+});
+
+
+/* HELPERS */
+const checkInputs = (req, res) => {
+  if (!ops[req.params.op] || isNaN(parseFloat(req.params.num1)) || isNaN(parseFloat(req.params.num2))) {
+    res.status(418);
+    res.send(error418);
+  }
+}
+
+// TODO Move to separate html file
+const error418 = `
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+  <head>
+    <meta name="author" content="Joseph P. Pasaoa" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Calculator Server | Error | Joseph P. Pasaoa</title>
+  </head>
+  <body>
+    
+        <h1>Error</h1>
+
+        <p>You took a wrong turn somewhere because now you're nowhere. Please check your URL inputs and reload the page.</p>
+
+        <ul>
+          <li><strong>to add: </strong>/add/num1/num2</li>
+          <li><strong>to subtract: </strong>/sub/num1/num2</li>
+          <li><strong>to multiply: </strong>/mul/num1/num2</li>
+          <li><strong>to divide: </strong>/div/num1/num2</li>
+        </ul>
+
+  </body>
+  </html>
+`;
