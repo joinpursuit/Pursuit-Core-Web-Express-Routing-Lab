e58a1fb9a993332e0f7bd072e95c0fd6a37c2da8
gsanchez13 <gisellesanchez@pursuit.org>
2019-10-12 11:25:45 -0400

    universal operations function

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -8,49 +8,92 @@ server.use(cors());
 server.get("/", (req, res) => {
     console.log("Welcome to my calculator API")
 })
-server.get("/add/:num1/:num2", (req, res) => {
+// server.get("/add/:num1/:num2", (req, res) => {
+//     let num1 = parseInt(req.params.num1);
+//     let num2 = parseInt(req.params.num2);
+//     if(isNaN(num1) || isNaN(num2)){
+//         res.send("Only Numbers Please!");
+//         return;
+//     }
+//     let result = mathMod.add(num1, num2);
+//     let obj = {
+//         "num1": num1,
+//         "num2": num2,
+//         "result": result
+//     }
+//     res.json(obj)
+// })
+// server.get("/sub/:num1/:num2", (req, res) => {
+//     let num1 = parseInt(req.params.num1);
+//     let num2 = parseInt(req.params.num2);
+//     if(isNaN(num1) || isNaN(num2)){
+//         res.send("Only Numbers Please!");
+//         return;
+//     }
+//     let result = mathMod.sub(num1, num2);
+//     let obj = {
+//         "num1": num1,
+//         "num2": num2,
+//         "result": result
+//     }
+//     res.json(obj);
+// })
+// server.get("/mul/:num1/:num2", (req, res) => {
+//     let num1 = parseInt(req.params.num1);
+//     let num2 = parseInt(req.params.num2);
+//     if(isNaN(num1) || isNaN(num2)){
+//         res.send("Only Numbers Please!");
+//         return;
+//     }
+//     let result = mathMod.mul(num1, num2);
+//     let obj = {
+//         "num1": num1,
+//         "num2": num2,
+//         "result": result
+//     }
+//     res.json(obj)
+// })
+// server.get("/div/:num1/:num2", (req, res) => {
+//     let num1 = parseInt(req.params.num1);
+//     let num2 = parseInt(req.params.num2);
+//     if(isNaN(num1) || isNaN(num2)){
+//         res.send("Only Numbers Please!");
+//         return;
+//     }
+//     let result = mathMod.div(num1, num2);
+//     let obj = {
+//         "num1": num1,
+//         "num2": num2,
+//         "result": result
+//     }
+//     res.json(obj)
+// })
+server.get('/:operator/:num1/:num2', (req, res) => {
     let num1 = parseInt(req.params.num1);
     let num2 = parseInt(req.params.num2);
-    let result = mathMod.add(num1, num2);
-    let obj = {
-        "num1": num1,
-        "num2": num2,
-        "result": result
+    let operator = req.params.operator;
+    if(isNaN(num1) || isNaN(num2)){
+        res.send("Only Numbers Please!");
+        return;
     }
-    res.json(obj)
-})
-server.get("/sub/:num1/:num2", (req, res) => {
-    let num1 = parseInt(req.params.num1);
-    let num2 = parseInt(req.params.num2);
-    let result = mathMod.sub(num1, num2);
-    let obj = {
-        "num1": num1,
-        "num2": num2,
-        "result": result
+    const operations = {
+        "add": mathMod.add,
+        "sub": mathMod.sub,
+        "mul": mathMod.mul,
+        "div": mathMod.div,
     }
-    res.json(obj);
-})
-server.get("/mul/:num1/:num2", (req, res) => {
-    let num1 = parseInt(req.params.num1);
-    let num2 = parseInt(req.params.num2);
-    let result = mathMod.mul(num1, num2);
-    let obj = {
-        "num1": num1,
-        "num2": num2,
-        "result": result
+    if(operations[operator]){
+        let result = operations[operator](num1, num2);
+        let obj = {
+            "num1": num1,
+            "num2": num2,
+            "result": result
+        }
+        res.json(obj)
     }
-    res.json(obj)
-})
-server.get("/div/:num1/:num2", (req, res) => {
-    let num1 = parseInt(req.params.num1);
-    let num2 = parseInt(req.params.num2);
-    let result = mathMod.div(num1, num2);
-    let obj = {
-        "num1": num1,
-        "num2": num2,
-        "result": result
+    else {
+        res.send("Invalid operation")
     }
-    res.json(obj)
 })
 server.use("*", () => {
     console.log("Hi, that function doesn't exist, as this server can only add, subtract, divide and multiply! 404!")
