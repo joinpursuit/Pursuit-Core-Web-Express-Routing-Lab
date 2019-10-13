83b0023a780b2a86248e19671ae059cb56209e9f
karenm09 <karenm929@hotmail.com>
2019-10-13 09:41:32 -0400

    thought I submitted..oops

--- calculator.js
+++ calculator.js
@@ -1,42 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let resBtn = document.querySelector('#res-btn')
-    resBtn.addEventListener('click', loadDataFromServer)
-})
-
-async function loadDataFromServer() {
-    const url = ("http://localhost:8000/")
-    const resp = await axios.get(url)    
-    displayDataFromServer(resp.data)
-}
-
-const displayDataFromServer = (data) =>{
-    let num1 = document.querySelector('#num1').value
-    let num2 = document.querySelector('#num2').value
-    console.log(num1)
-    console.log(num2)
-}
-
-
-addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () =>{
     console.log("content loaded")
     setupButtonListener()
  })
  const setupButtonListener = () =>{
-    const calculate = document.querySelector("form")
+    const calculate = document.querySelector("res-btn")
     calculate.addEventListener("submit",fetchServerData)
  }
- function fetchServerData(event){
+ const fetchServerData = (event) => {
     event.preventDefault();
  
     let operator = document.querySelector("#operator").value;
-    let number1 = document.querySelector("#number1").value;
-    let number2 = document.querySelector("#number2").value;
+    let number1 = document.querySelector("#num1").value;
+    let number2 = document.querySelector("#num2").value;
  
-    let url = `http://localhost:8004/${operator}/${number1}/${number2}`
+    let url = `http://localhost:8000/${operator}/${number1}/${number2}`
     fetch(url).then(response => {
         return response.json()
     }).then(total =>{
-        let answer = document.querySelector("#answer")
+        let answer = document.querySelector("#result")
         answer.innerText = `${total.answer}`
     })
  }
