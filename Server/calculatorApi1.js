653c38843ee4242d1699efe24f8a992397759e05
your name here <your_email_here>
2019-10-13 20:32:10 -0400

    Savitas Express Routing add

--- Server/calculatorApi1.js
+++ Server/calculatorApi1.js
@@ -2,9 +2,17 @@ const express = require ('express')
 const app = express()
 const port = 8080
 
-app.get('/', (res,req) => {
+app.get('/', (req,res) => {
+    res.send("the server is working")
 
+})
 
+app.get('/add/:num1/:num2', (req, res) => {
+        let num1 = req.params.num1
+        let num2 = req.params.num2
+        let result = parseInt(num1) + parseInt(num2)
+        res.json(result)
+        
 })
 
 app.listen(port, () => {
