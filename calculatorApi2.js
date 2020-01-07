c6f71c37f151f7a51298bc5b0174ebb8d199c85a
John Jones Jr <jayjohnjones@gmail.com>
2020-01-06 22:01:41 -0500

    Calc

---
+++ calculatorApi2.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("hello")
+    let num1 = document.querySelector("#num1")
+    let num2 = document.querySelector("#num2")
+    let op = document.querySelector("#op")
+    let btn = document.querySelector("button")
+    let form = document.querySelector("form")
+
+   form.addEventListener("submit", () => {
+       debugger
+   })
+
+    // axios.get("http://localhost:3000").then(res =>{
+    //     res.data
+    // })
+
+})
