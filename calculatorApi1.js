8ad65c947b8b11d676992fde68eba2dba0990aad
Kelvin <kelvinarellano@pursuit.org>
2020-01-06 21:39:07 -0500

    initial functions done

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -3,3 +3,19 @@ const cors = require("cors");
 const app = express();
 app.use(cors())
 const port = 3000
+
+const add = (arg1, arg2) =>{
+    return parseInt(arg1) + parseInt(arg2)
+}
+
+const sub = (arg1,arg2) => {
+    return parseInt(arg1) - parseInt(arg2)
+}
+
+const div = (arg1,arg2) => {
+    return parseInt(arg1) / parseInt(arg2)
+}
+
+const mult = (arg1,arg2) => {
+    return parseInt(arg1) * parseInt(arg2)
+}
