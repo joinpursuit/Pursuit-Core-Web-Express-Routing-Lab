701cd14e7d0cd59f482f34a45112abd973d47e28
Kelvin <kelvinarellano@pursuit.org>
2020-01-06 21:49:46 -0500

    back end functions added

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -19,3 +19,27 @@ const div = (arg1,arg2) => {
 const mult = (arg1,arg2) => {
     return parseInt(arg1) * parseInt(arg2)
 }
+
+app.get("/", (req,res)=>{
+    res.send("You made a request at /url")
+})
+
+app.get("/add/:num1/:num2",(req,res)=>{ 
+    res.json(add(req.params.num1,req.params.num2))
+})
+
+app.get("/mult/:num1/:num2",(req,res)=>{  
+    res.json(mult(req.params.num1,req.params.num2))
+})
+
+app.get("/div/:num1/:num2",(req,res)=>{
+    res.json(div(req.params.num1,req.params.num2))
+})
+
+app.get("/sub/:num1/:num2",(req,res)=>{ 
+    res.json(sub(req.params.num1,req.params.num2))
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is runing on ${port}`)
+})
