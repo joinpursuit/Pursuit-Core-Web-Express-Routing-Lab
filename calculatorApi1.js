2661eb432d02bf709e38f1623c6f9ee823964e17
Ohidurr <Ohidurrahman96@gmail.com>
2020-01-06 21:57:22 -0500

    set up express

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -4,13 +4,35 @@ const cors = require("cors")
 app.use(cors())
 const port = 3000;
 
+const calc =  {
+        num: {
+            num1: x,
+            num2: y
+        }
+    }
+
+
 
 
 app.get("/", (req,res)=>{
 
-    res.send("YOU made a get requst at /url")
+ res.send("YOU made a get requst at /url")
 })
+app.get("/add/:num1/:num2", (req,res) => {
+    console.log(req.params.num1);
+    console.log(req.params.num2);
+    res.json(add[req.params.num])
+    
+})
+app.get("/sub/:num1/:num2", (req,res) => {
 
+})
+app.get("/mul/:num1/:num2", (req,res) => {
+
+})
+app.get("/div/:num1/:num2", (req,res) => {
+
+})
 app.listen(port,()=>{
 console.log("server is running on Port:", port)
 
