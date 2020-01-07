db79624b1ffbc90fb5202bb3dd5bb23a426aff81
Nilber Remon <nilberremon@gmail.com>
2020-01-06 21:45:10 -0500

    Backend Complete

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -5,12 +5,18 @@ const app = express()
 app.use(cors())
 const port = 3001
 
-app.get("/", (req, res) => {
-    res.send("You made a GET request at / url")
+app.get("/:parameter", (req, res) => {
+    res.send("This API performs calculations based on parameters received.")
 })
 
 app.get("/add/:num1/:num2", (req, res) => {
-    res.json({num1: req.params.num1, num2: req.params.num2, result: parseInt(req.params.num1)+parseInt(req.params.num2)})
+    let num1 = parseInt(req.params.num1)
+    let num2 = parseInt(req.params.num2)
+    if (isNaN(num1) || isNaN(num2)){
+        res.send("Numbers Only")
+    } else {
+        res.json({num1: num1, num2: num2, result: num1+num2})
+    }
 })
 
 app.get("/multiply/:num1/:num2", (req, res) => {
