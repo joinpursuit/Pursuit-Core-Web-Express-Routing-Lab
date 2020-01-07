3192b2f5bcd87f1c90e45d3e27ef46be34eab5e5
Farrah Isabelle <farrahrios@pursuit.org>
2020-01-06 21:15:30 -0500

    first commit

---
+++ calculatorApi1.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+const cors = require("cors")
+app.use(cors())
+const port = 3000;
+
+
+
+app.get("/", (req,res)=>{
+
+    res.send("YOU made a get requst at /url")
+})
+
+app.listen(port,()=>{
+console.log("server is running on Port")
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const people = {
+//     1:{name: "Corey", id: 1}, 
+//     2:{name: "Jhenya", id: 2}, 
+//     3:{name: "John", id: 3}
+// }
+
+// app.get("/", (req,res)=>{
+
+//     res.send("YOU made a get requst at /url")
+// })
+
+// app.get("/people", (req, res)=>{
+//     res.json
+//     (people)
+// })
+
+// app.get("/people/:id", (req,res)=>{
+//     console.log(req.params.id);
+//     res.json(people[req.params.id])
+// })
+
+// app.post("/",(req,res)=>{
+//     res.send("You mand a Post reauwest")
+// })
+
+// app.listen(port, ()=>{
+//     console.log("Server is runing on port", port);
+// })
+
