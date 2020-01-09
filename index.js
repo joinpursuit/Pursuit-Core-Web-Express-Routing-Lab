91625978561bdeeb59250474ef212932d832c4c4
Kevin Wong <kevinwong@pursuit.org>
2020-01-09 01:46:19 -0500

    build html front end for calculator, create back end to build data

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let body = document.querySelector("body");
+    let num1 = document.querySelector("#num1");
+    let num2 = document.querySelector("#num2");
+    let form = document.querySelector("form");
+    let select = document.querySelector("#selector");
+    let h2 = document.querySelector("h2")
 
-    let h1 = document.createElement("h1");
-    h1.innerText = "Calculator";
-    body.appendChild(h1);
-
-    let num1 = document.createElement("input");
-    let select = document.createElement("select");
-    let num2 = document.createElement("input");
-    num1.type = "number";
-    num1.min = "0";
-    num1.max = "99";
-    num2.type = "number";
-    num2.min = "0";
-    num2.max = "99";
-
-    let p = document.createElement("p");
-    
-
-    let btn = document.createElement("input");
-    btn.type = "submit";
-    btn.innerText = "Calculate"
-
-    body.appendChild(num1);
-    body.appendChild(select);
-    body.appendChild(num2);
-    body.appendChild(p)
-    body.appendChild(btn);
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        axios.get(`http://localhost:3000/add/${num1.value}/${num2.value}/`).then(res => {
+            console.log(select.val);
+            h2.innerText = res.data.result;
+        })
+    })
+   
 
     
 })
