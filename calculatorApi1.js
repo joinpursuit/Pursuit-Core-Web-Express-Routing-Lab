f1243bde225b150d2018c2175d4b787bade95824
Jay Fowler <jcafowler@gmail.com>
2020-01-08 19:07:52 -0500

    Create files

---
+++ calculatorApi1.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const cors = require("cors")
+const app = express();
+const port = 3000
+app.use(cors())
+
+app.get("/add/:num1/:num2", (req, res) => {
+
+})
+app.get("/sub/:num1/:num2", (req, res) => {
+
+})
+app.get("/mul/:num1/:num2", (req, res) => {
+
+})
+app.get("/div/:num1/:num2", (req, res) => {
+  
+})
