0c80c4c93044bf934f2ceaa86a4cb2641e32d5ce
Marvin Thompson <marvinthompson@pursuit.org>
2020-01-08 19:39:42 -0500

    more incoming changes

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -26,21 +26,6 @@ app.get("/add", (req, res) => {
 
 app.get("/add/:num1/:num2/", (req, res) => {
     res.json({num1: req.params.num1, num2: req.params.num2, result: add(req.params.num1, req.params.num2)}) 
-<<<<<<< HEAD
-})
-
-app.get("/sub/:num1/:num2/", (req, res) => {
-    res.json({num1: req.params.num1, num2: req.params.num2, result:sub(req.params.num1, req.params.num2)})
-})
-
-app.get("/mul/:num1/:num2/", (req, res) => {
-    res.json({num1: req.params.num1, num2: req.params.num2, result:mul(req.params.num1, req.params.num2)})
-})
-
-app.get("/div/:num1/:num2/", (req, res) => {
-    res.json({num1: req.params.num1, num2: req.params.num2, result:div(req.params.num1, req.params.num2)})
-})
-=======
 })
 app.get("/sub/:num1/:num2/", (req, res) => {
     res.json({num1: req.params.num1, num2: req.params.num2, result:sub(req.params.num1, req.params.num2)})
@@ -51,7 +36,6 @@ app.get("/mul/:num1/:num2/", (req, res) => {
 app.get("/div/:num1/:num2/", (req, res) => {
     res.json({num1: req.params.num1, num2: req.params.num2, result:div(req.params.num1, req.params.num2)})
 })
->>>>>>> 61fe8ea282c3cd670f4dea2fc5850655623a006e
 
 app.listen(port, () => {
     console.log("Server is running on Port " + port)
