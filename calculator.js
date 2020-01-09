d5657d5be32529aead203a55704d300cde98849f
Kevin Brutus <kevinbrutus@pursuit.org>
2020-01-08 20:15:12 -0500

    edit result.innerText in calculator.js file

--- calculator.js
+++ calculator.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector("form");
   let n1 = document.querySelector("#num1").value
   let n2 = document.querySelector("#num2").value
-  let meth = document.querySelector("option").value
+  let option = document.querySelector("option")
+  let result = document.querySelector("#result")
   
   form.addEventListener("submit", (event) => {
     event.preventDefault()
-    axios.get(`http://localhost:3000/:${meth}/:${n1}/${n2}`).then(res => {
-      debugger
-    let result = document.querySelector("div")
-    // result.innerText = 
+    axios.get(`http://localhost:3000/${option.value}/${n1.value}/${n2.value}`).then(res => {
+      // debugger
+    result.innerText = res.data.result
     })
   })
 
