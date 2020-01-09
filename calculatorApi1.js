33c144e326adb7bb42b7a5fa4b103e012a431500
Jay Fowler <jcafowler@gmail.com>
2020-01-08 20:04:55 -0500

    Refine error handling on backend

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -4,9 +4,6 @@ const app = express();
 const port = 3000
 app.use(cors())
 
-app.get("/:param", (req, res) => {
-  res.send("Please navigate to '/add', '/sub', '/mul', or '/div' followed by two numbers to use this API. i.e. .../add/num1/num2")
-})
 
 app.get("/add/:num1/:num2", (req, res) => {
   if (!parseInt(req.params.num1) || !parseInt(req.params.num2)){
@@ -53,6 +50,10 @@ app.get("/div/:num1/:num2", (req, res) => {
     nums.result = nums.num1 / nums.num2
     res.json(nums)
   }
+})
+
+app.get("*", (req, res) => {
+  res.send("Please navigate to '/add', '/sub', '/mul', or '/div' followed by two numbers to use this API. i.e. .../add/num1/num2")
 })
 
 app.listen(port, () => {
