acc67287d66bacba74409101aa70b83e5f9f597c
Jay Fowler <jcafowler@gmail.com>
2020-01-08 19:30:35 -0500

    Build Calculators

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -5,14 +5,34 @@ const port = 3000
 app.use(cors())
 
 app.get("/add/:num1/:num2", (req, res) => {
-
+  let nums = req.params
+  nums.num1 = parseInt(nums.num1)
+  nums.num2 = parseInt(nums.num2)
+  nums.result = nums.num1 + nums.num2
+  res.json(nums)
 })
 app.get("/sub/:num1/:num2", (req, res) => {
-
+  let nums = req.params
+  nums.num1 = parseInt(nums.num1)
+  nums.num2 = parseInt(nums.num2)
+  nums.result = nums.num1 - nums.num2
+  res.json(nums)
 })
 app.get("/mul/:num1/:num2", (req, res) => {
-
+  let nums = req.params
+  nums.num1 = parseInt(nums.num1)
+  nums.num2 = parseInt(nums.num2)
+  nums.result = nums.num1 * nums.num2
+  res.json(nums)
 })
 app.get("/div/:num1/:num2", (req, res) => {
-  
+  let nums = req.params
+  nums.num1 = parseInt(nums.num1)
+  nums.num2 = parseInt(nums.num2)
+  nums.result = nums.num1 / nums.num2
+  res.json(nums)
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
