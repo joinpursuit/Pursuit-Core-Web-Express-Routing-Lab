f89a21c29d72621a8f91eb8cde5756ccc5489827
your name here <your_email_here>
2020-01-08 23:29:18 -0500

    finished Front End

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-    const calc = () =>{
-        console.log("math1")
-    let button = document.querySelector("button")
-       let select = document.querySelector("select")
-       debugger
-       let math=select.selectedOptions[0].value
-       if(select.selectedOptions[0].value){
-        let num1 = document.getElementById("num1")
-        let num2 = document.getElementById("num2")
-        axios.get(`http://localhost:3000/${math}/${num1.value}/${num2.value}`).then(res=>{
-           let p =document.querySelector("p")
-            p.innerText= res.data
-        })
+let button = document.querySelector("button")
+
+button.addEventListener("click",()=>{
+    let select = document.querySelector("select")
+    let math=select.selectedOptions[0].value
+    if(select.selectedOptions[0].value){
+     let num1 = document.getElementById("num1")
+     let num2 = document.getElementById("num2")
+     axios.get(`http://localhost:3000/${math}/${num1.value}/${num2.value}`).then(res=>{
+        let p =document.querySelector("p")
+         p.innerText= res.data
+     })
     }
-}
-calc()
+})
+
