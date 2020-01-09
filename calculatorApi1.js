ca8911ad6dbf3707656f5643778b9d313842ff5e
Farrah Isabelle <farrahrios@pursuit.org>
2020-01-08 19:14:49 -0500

    test

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -3,6 +3,13 @@ const app = express();
 const cors = require("cors")
 app.use(cors())
 const port = 3000;
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
 
 const calc =  {
         num: {
@@ -71,36 +78,4 @@ console.log("server is running on Port:", port)
 
 
 
-
-
-
-
-// const people = {
-//     1:{name: "Corey", id: 1}, 
-//     2:{name: "Jhenya", id: 2}, 
-//     3:{name: "John", id: 3}
-// }
-
-// app.get("/", (req,res)=>{
-
-//     res.send("YOU made a get requst at /url")
-// })
-
-// app.get("/people", (req, res)=>{
-//     res.json
-//     (people)
-// })
-
-// app.get("/people/:id", (req,res)=>{
-//     console.log(req.params.id);
-//     res.json(people[req.params.id])
-// })
-
-// app.post("/",(req,res)=>{
-//     res.send("You mand a Post reauwest")
-// })
-
-// app.listen(port, ()=>{
-//     console.log("Server is runing on port", port);
-// })
 
