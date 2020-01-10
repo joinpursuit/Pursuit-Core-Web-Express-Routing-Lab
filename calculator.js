bfab0f18bfab0ab32b1b652bfb69f6f052028585
bsihame <sihamebazi@pursuit.org>
2020-01-10 00:55:54 -0500

    fix caculatorjs

--- calculator.js
+++ calculator.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector("form");
-  let n1 = document.querySelector("#num1").value
-  let n2 = document.querySelector("#num2").value
-  let option = document.querySelector("option")
+  let n1 = document.querySelector("#num1")
+  let n2 = document.querySelector("#num2")
+  let select = document.querySelector("select")
   let result = document.querySelector("#result")
   
   form.addEventListener("submit", (event) => {
+    debugger
     event.preventDefault()
-    axios.get(`http://localhost:8000/${option.value}/${n1.value}/${n2.value}`).then(res => {
+    axios.get(`http://localhost:8000/${select.value}/${n1.value}/${n2.value}`).then(res => {
       // debugger
     result.innerText = res.data.result
     })
