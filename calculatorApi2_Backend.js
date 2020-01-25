43ab2e85163d29a66c088de3c4bccc3cfa2a1d8e
Nilber Remon <nilberremon@gmail.com>
2020-01-25 17:58:20 -0500

    Express Routing Lab Complete

--- calculatorApi2_Backend.js
+++ calculatorApi2_Backend.js
@@ -7,15 +7,36 @@ const port = 3000;
 
 app.use(cors());
 
-app.get("/:op/:num1/:num2", (req, res) => {
-    let operator = req.params.op
+const add = (num1,num2) => parseInt(num1) + parseInt(num2);
+const subtract = (num1, num2) => parseInt(num1) - parseInt(num2);
+const multiply = (num1,num2) => parseInt(num1) * parseInt(num2);
+const divide = (num1,num2) => parseInt(num1) / parseInt(num2);
+
+const calculate = (operator, num1, num2) => {
+    if (operator === "add"){
+        return add(num1,num2)
+        console.log(add(num1,num2))
+    } else if (operator === "subtract"){
+        return subtract(num1,num2)
+    } else if (operator === "multiply"){
+        return multiply(num1,num2)
+    } else if (operator === "divide"){
+        return divide(num1,num2)
+    } else if (isNan(num1) || isNan(num2)){
+        res.status(400).send("Please input valid numbers")
+    }
+}
+
+app.get("/:operator/:num1/:num2", (req, res) => {
+    let operator = req.params.operator
     let num1 = req.params.num1;
     let num2 = req.params.num2;
-    
+    let calc = calculate(operator, num1,num2)
+
     let answer = {
         num1: num1,
         num2: num2,
-        result: parseInt(num1) + operator + parseInt(num2)
+        calculation: calc
     }
     
     res.status(200)
@@ -23,5 +44,5 @@ app.get("/:op/:num1/:num2", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+    console.log(`Server is running on ${port}`)
 })
