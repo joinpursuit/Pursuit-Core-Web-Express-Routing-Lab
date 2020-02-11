0bef24ebad7a6c6005aade926ccb8fa4935b6bd2
Wilsantos1975 <faustosantos@pursuit.org>
2020-02-11 11:22:15 -0500

    complete backend routes

--- calculatorApi1.js
+++ calculatorApi1.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000
 app.use(cors())
 
+app.get("/", (req, res) => {
+  res.send("Please navigate to '/add', '/sub', '/mul', or '/div' followed by two numbers to use this API. i.e. .../add/num1/num2")
+})
 
 app.get("/add/:num1/:num2", (req, res) => {
   if (!parseInt(req.params.num1) || !parseInt(req.params.num2)){
@@ -20,7 +23,6 @@ app.get("/sub/:num1/:num2", (req, res) => {
   if (!parseInt(req.params.num1) || !parseInt(req.params.num2)){
     res.send("Please use numbers only.")
   } else {
-    console.log(parseInt(req.params.num1))
     let nums = req.params
     nums.num1 = parseInt(nums.num1)
     nums.num2 = parseInt(nums.num2)
@@ -52,9 +54,6 @@ app.get("/div/:num1/:num2", (req, res) => {
   }
 })
 
-app.get("*", (req, res) => {
-  res.send("Please navigate to '/add', '/sub', '/mul', or '/div' followed by two numbers to use this API. i.e. .../add/num1/num2")
-})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
